68400a5c86573e45058182b99780048df3d80df6
Belito-Goncalves <150486039+Belito-Goncalves@users.noreply.github.com>
2023-11-30 13:41:54 +0000

    Add files via upload

---
+++ Teams.js
@@ -0,0 +1,15 @@
+function showLoading() {
+    $("#myModal").modal('show', {
+        backdrop: 'static',
+        keyboard: false
+    });
+}
+function hideLoading() {
+    $('#myModal').on('shown.bs.modal', function (e) {
+        $("#myModal").modal('hide');
+    })
+}
+
+$(document).ajaxComplete(function (event, xhr, options) {
+    $("#myModal").modal('hide');
+})
